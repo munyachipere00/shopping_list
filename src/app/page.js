39f75a372f626ec4338897b763cbbc918e008510
Light skinned temptation <mac2019@Mikes-MacBook-Air.local>
2024-11-02 19:43:12 +0200

    new commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,175 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
-export default function Home() {
+'use client';
+import { useState } from "react";
+import { editItem } from "./edit";
+//import { updateItem } from "./update";
+
+
+
+const App = () => {
+  const [list, setList] = useState([]);
+  const [value, setValue] = useState("");
+  const [buttonval, setbuttonval] = useState("Add");
+  const  [thiskey, setthiskey] = useState();
+
+  
+
+  const addItem = () => {
+    setList([...list, { key: list.length + 1, title: value, isChecked: false, isComplete : false }]);
+    setValue("");
+    
+  };
+  
+  const deleteItem = (ids) => {
+    const sortedList = list.filter((item) => item.key !== ids);
+    setList(sortedList);
+  };
+
+  const checkItem = (ids) => {
+    setList(
+      list.map((item) =>
+        item.key === ids ? { ...item, isChecked: !item.isChecked } : item
+      )
+    );
+  };
+
+  const decision = (thiskey) => {
+    if (buttonval == "Add"){ 
+      addItem();
+    }
+    else{
+      updateItem(thiskey);
+    }
+   
+  };
+
+
+
+  const updateItem = (ids) => {
+    setList(
+      list.map((item) =>
+        item.key === ids ? { ...item, title: value } : item
+      )
+    );
+    setValue("");
+    setbuttonval("Add")
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="container">
+      <h1 className="title">To do List</h1>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className="input-container">
+        <input
+          type="text"
+          className="item-input"
+          placeholder="Add an item"
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+        />
+
+
+        <button className="add-button" 
+        onClick={() => decision(thiskey)}>
+           {buttonval}
+        </button>
+      </div>
+
+      <ul className="item-list">
+
+
+      {list.map((item) => item.isChecked === false ? 
+        (
+          <li className="item"
+          key={item.key}
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <span
+              style={{
+                textDecoration: item.isChecked ? "line-through" : "none",
+              }}
+            >
+              {item.title}
+            </span>
+             
+
+            
+
+            <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+            
+              <input
+                type="checkbox"
+                name=""
+                id=""
+                onChange={() => checkItem(item.key)}
+              />
+              <p>Done?</p>
+
+              <button
+                className="delete-button"
+                onClick={() => editItem(item.key, item, list, setList , value, setValue, buttonval, setbuttonval, setthiskey,  thiskey)}
+              >
+                Edit
+              </button>
+             
+
+              <button
+                className="delete-button"
+                onClick={() => deleteItem(item.key)}
+              >
+                Delete
+              </button>
+
+
+            </div>
+          </li>
+        )
+        : ""
+        )
+        }
+
+        
+      
+
+      </ul>     
+
+
+     <h2>Competed</h2>
+
+      <ul className="item-list">
+
+
+        {list.map((item) => item.isChecked === true ? 
+        (
+          <li className="item" key={item.key}>
+            <span
+              style={{
+                textDecoration: item.isChecked ? "line-through" : "none",
+              }}
+            >
+              {item.title}
+            </span>
+            
+            <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+              <input
+                type="checkbox"
+                name=""
+                id=""
+                onChange={() => checkItem(item.key)}
+              />
+              </div>
+              <p>Undo</p>
+
+          </li>
+        ) : ""
+        )
+        }
+      
+
+      </ul>       
+
     </div>
+   
   );
-}
+};
+
+export default App;
